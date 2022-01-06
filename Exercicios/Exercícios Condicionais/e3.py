216a77aa491ab7dd26b9a79a8c2f2b7ea6104420
IgorTorquatto <igor.torquato.20@gmail.com>
2022-01-06 17:35:45 -0300

    Novos Arquivos

--- Exercicios/Exercícios Condicionais/e3.py
+++ Exercicios/Exercícios Condicionais/e3.py
@@ -2,3 +2,10 @@
 Faça um Programa que peça um valor e mostre na tela
 se o valor é positivo ou negativo.
 """
+valor=int(input("Digite um valor: "))
+
+if valor>=0:{
+    print("O número é positivo.")
+}
+else:
+    print("O número é negativo.")
